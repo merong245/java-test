5020c694df0a1bc59ede875f61635cc45dabd87b
이준혁 <merong245@gmail.com>
2024-03-02 21:13:15 +0900

    [Feat] : 반복하는 테스트 인자가 여러개 일 때, Aggregator 활용 및 커스텀 Aggregator 적용

--- src/main/java/me/hol22mol22/javatestlearn/study/Study.java
+++ src/main/java/me/hol22mol22/javatestlearn/study/Study.java
@@ -6,11 +6,30 @@ public class Study {
 
     private int limit;
 
+    private String name;
+
     public Study(int limit){
         if(limit < 0){
             throw new IllegalArgumentException("limit 은 0이상이어야함");
         }
         this.limit = limit;
+    }
+
+    public Study(int limit, String name){
+        if(limit < 0){
+            throw new IllegalArgumentException("limit 은 0이상이어야함");
+        }
+        this.limit = limit;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Study{" +
+                "status=" + status +
+                ", limit=" + limit +
+                ", name='" + name + '\'' +
+                '}';
     }
 
     public StudyStatus getStatus(){
@@ -20,4 +39,6 @@ public class Study {
     public int getLimit() {
         return limit;
     }
+
+
 }
--- src/test/java/me/hol22mol22/javatestlearn/study/StudyTest.java
+++ src/test/java/me/hol22mol22/javatestlearn/study/StudyTest.java
@@ -4,7 +4,12 @@ import me.hol22mol22.javatestlearn.FastTest;
 import me.hol22mol22.javatestlearn.SlowTest;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.condition.*;
+import org.junit.jupiter.api.extension.ParameterContext;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.aggregator.AggregateWith;
+import org.junit.jupiter.params.aggregator.ArgumentsAccessor;
+import org.junit.jupiter.params.aggregator.ArgumentsAggregationException;
+import org.junit.jupiter.params.aggregator.ArgumentsAggregator;
 import org.junit.jupiter.params.converter.ArgumentConversionException;
 import org.junit.jupiter.params.converter.ConvertWith;
 import org.junit.jupiter.params.converter.SimpleArgumentConverter;
@@ -40,26 +45,57 @@ class StudyTest {
 
     @DisplayName("반복 테스트")
     @RepeatedTest(value = 10, name = "{displayName}, {currentRepetition}/{totalRepetitions}")
-    void repeat_test_10(RepetitionInfo repetitionInfo){
+    void repeat_test_10(RepetitionInfo repetitionInfo) {
         System.out.println("test" + repetitionInfo.getCurrentRepetition() + "/" +
                 repetitionInfo.getTotalRepetitions());
     }
 
     @DisplayName("반복 테스트, 파라미터")
     @ParameterizedTest(name = "{index} {displayName} message={0}")
-    @ValueSource(strings = {"날씨가","많이", "추웡"})
-    void parameterized_test(String message){
+    @ValueSource(strings = {"날씨가", "많이", "추웡"})
+    void parameterized_test(String message) {
         System.out.println("message = " + message);
     }
 
     @DisplayName("반복 테스트, 정수")
     @ParameterizedTest(name = "{index} {displayName} message={0}")
     @ValueSource(ints = {10, 20, 50})
-    void parameterized_test_with_int(@ConvertWith(StudyConverter.class) Study study){
+    void parameterized_test_with_int(@ConvertWith(StudyConverter.class) Study study) {
         System.out.println("message = " + study.getLimit());
     }
 
-    static class StudyConverter extends SimpleArgumentConverter{
+    @DisplayName("반복 테스트, 정수")
+    @ParameterizedTest(name = "{index} {displayName} message={0}")
+    @CsvSource({"10, '자바스터디'", "20, '스프링"})
+    void parameterized_test_with_csv(Integer limit, String name) {
+        Study study = new Study(limit, name);
+        System.out.println(study);
+    }
+
+    @DisplayName("반복 테스트, 정수")
+    @ParameterizedTest(name = "{index} {displayName} message={0}")
+    @CsvSource({"10, '자바스터디'", "20, '스프링"})
+    void parameterized_test_with_csv_using_aggregator(ArgumentsAccessor argumentsAccessor) {
+        Study study = new Study(argumentsAccessor.getInteger(0), argumentsAccessor.getString(1));
+        System.out.println(study);
+    }
+
+    @DisplayName("반복 테스트, 정수")
+    @ParameterizedTest(name = "{index} {displayName} message={0}")
+    @CsvSource({"10, '자바스터디'", "20, '스프링"})
+    void parameterized_test_with_csv_using_custom_aggregator(@AggregateWith(StudyAggregator.class) Study study) {
+        System.out.println(study);
+    }
+
+    static class StudyAggregator implements ArgumentsAggregator {
+
+        @Override
+        public Object aggregateArguments(ArgumentsAccessor argumentsAccessor, ParameterContext parameterContext) throws ArgumentsAggregationException {
+            return new Study(argumentsAccessor.getInteger(0), argumentsAccessor.getString(1));
+        }
+    }
+
+    static class StudyConverter extends SimpleArgumentConverter {
 
         @Override
         protected Object convert(Object o, Class<?> aClass) throws ArgumentConversionException {
@@ -71,19 +107,19 @@ class StudyTest {
     @DisplayName("반복 테스트, 다수 파라미터")
     @ParameterizedTest(name = "{index} {displayName} message={0}")
     @CsvSource({"java, 10", ""})
-    void parameterized_test_with_cvs_source(String message){
+    void parameterized_test_with_cvs_source(String message) {
         System.out.println("message = " + message);
     }
 
     @FastTest
     @DisplayName("테스트 만들기 fast")
-    void create_test_tag_fast(){
+    void create_test_tag_fast() {
         System.out.println("im fast");
     }
 
     @SlowTest
     @DisplayName("테스트 만들기 slow")
-    void create_test_tag_slow(){
+    void create_test_tag_slow() {
         System.out.println("im slow");
     }
 
@@ -135,6 +171,7 @@ class StudyTest {
     void create_test_with_assume() {
         System.out.println("im here");
     }
+
     @Test
     @DisplayName("조건에 따라 테스트 실행 - 특정 시스템 환경에서")
     void create_test_with_assume_on_testenv() {
@@ -161,7 +198,6 @@ class StudyTest {
     void create_test_with_assume_over_jdk8() {
         System.out.println("JDK over 8");
     }
-
 
 
     @BeforeAll
